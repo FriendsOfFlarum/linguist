0905ba47688b4d25af9738aeed4eaa8086ef3293
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-10-03 23:25:48 +0200

    chore: mediocre IQ TS refactor

--- js/src/admin/models/TextString.ts
+++ js/src/admin/models/TextString.ts
@@ -1,12 +1,13 @@
 import Model from "flarum/common/Model";
 
 export default class TextString extends Model {
-    key = Model.attribute("key");
-    locale = Model.attribute("locale");
-    value = Model.attribute("value");
+    key = Model.attribute<string>("key");
+    locale = Model.attribute<string>("locale");
+    value = Model.attribute<string>("value");
 
     apiEndpoint() {
         return (
+            // @ts-ignore
             "/fof/linguist/strings" + (this.exists ? "/" + this.data.id : "")
         );
     }
